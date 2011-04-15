598f5eb98dd6f6f5e1bc337de519ac3173b02c6f
trampus <trampus@gmail.com>
2011-04-15 11:25:19 -0700

    cleaning up the test code now that the code base is getting more structored.

--- src/SodaSuite.java
+++ src/SodaSuite.java
@@ -1,16 +1,5 @@
-import java.io.File;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
-
-import soda.SodaElementsList;
-import soda.SodaEvents;
-import soda.SodaReporter;
 import soda.SodaSupportedBrowser;
 import soda.SodaTest;
-import soda.SodaTypes;
-import soda.SodaXML;
 
 public class SodaSuite {
 
@@ -18,35 +7,17 @@ public class SodaSuite {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SodaXML sodaXml = null;
-		SodaTypes sodaTypes = null;
-		SodaElementsList types = null;
 		String sodaTest = "/Users/trichmond/Documents/workspace/Soda-Project/src/test1.xml";
-		SodaEvents events = null;
-		SodaReporter rep = null;
 		SodaTest testobj = null;
 		
 		System.out.printf("Starting SodaSuite...\n");
 		
 		try {
-			/*
-			File tmp = new File(sodaTest);
-			String basename = tmp.getName();
-			basename = basename.substring(0, basename.length() -4);
-			rep = new SodaReporter(basename, null);
-			sodaXml = new SodaXML(sodaTest);
-			events = sodaXml.process();
-			sodaTypes = new SodaTypes();
-			types = sodaTypes.getTypes();
-			*/
-			
 			testobj = new SodaTest(sodaTest, SodaSupportedBrowser.FIREFOX);
-			
 		} catch(Exception exp) {
 			exp.printStackTrace();
 		}
 		
-		sodaXml = null;
 		System.out.printf("SodaSuite Finished.\n");
 	}
 
