4028c731fbb924d03219c223c65f33eeee351230
trampus <trampus@gmail.com>
2011-04-15 12:20:19 -0700

    starting to get the basic browser actions working.

--- src/soda/SodaBrowser.java
+++ src/soda/SodaBrowser.java
@@ -1,13 +1,54 @@
 package soda;
 
-public abstract class SodaBrowser {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
+public abstract class SodaBrowser implements SodaBrowserInterface {
+	
+	private WebDriver Driver = null;
+	
 	public SodaBrowser() {
 		
 	}
 	
 	public void newBrowser(SodaSupportedBrowser browserType) {
 		
+		try {
+			switch (browserType) {
+			case FIREFOX:
+				this.Driver = new FirefoxDriver();
+				break;
+			case IE:
+				this.Driver = new InternetExplorerDriver();
+				break;
+			case CHROME:
+				this.Driver = new ChromeDriver();
+			}
+			
+		} catch (Exception exp) {
+			exp.printStackTrace();
+		}
 	}
 	
+	public void refresh() {
+		this.Driver.navigate().refresh();
+	}
+	
+	public void forward() {
+		this.Driver.navigate().forward();
+	}
+	
+	public void back() {
+		this.Driver.navigate().back();
+	}
+	
+	public void close() {
+		this.Driver.close();
+	}
+	
+	public void url(String url) {
+		this.Driver.navigate().to(url);
+	}
 }
--- src/soda/SodaEventDriver.java
+++ src/soda/SodaEventDriver.java
@@ -3,9 +3,11 @@ package soda;
 public class SodaEventDriver {
 
 	private SodaEvents testEvents = null;
+	private SodaBrowser Browser = null;
 	
 	public SodaEventDriver(SodaBrowser browser, SodaEvents events) {
 		testEvents = events;
+		this.Browser = browser;
 	}
 	
 	public void processEvents(SodaEvents events) {
@@ -24,9 +26,39 @@ public class SodaEventDriver {
 	private boolean handleSingleEvent(SodaHash event) {
 		boolean result = false;
 		
+		switch ((SodaElements)event.get("type")) {
+		case BROWSER: 
+			result = browserEvent(event);
+			break;
+		}
 		
 		return result;
 	}
-			
+
+	private boolean browserEvent(SodaHash event) {
+		boolean result = false;
+		SodaBrowserActions browser_action = null;
+		
+		if (event.containsKey("action")) {
+			browser_action = SodaBrowserActions.valueOf(event.get("action").toString().toUpperCase());
+		}
+		
+		switch (browser_action) {
+		case REFRESH:
+			this.Browser.refresh();
+			break;
+		case CLOSE:
+			this.Browser.close();
+			break;	
+		case BACK:
+			this.Browser.back();
+			break;
+		case FORWARD:
+			this.Browser.forward();
+			break;
+		}
+		
+		return result;
+	}
 	
 }
